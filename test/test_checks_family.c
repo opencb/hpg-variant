50b027e1bab38c9808e86765a38931dc157f96b1
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-05-10 14:43:19 +0200

    Unit testing for mendelian errors checking finished
    Former-commit-id: 14f6fd706d62eeca0938fe9151585bdf49214c5c

--- test/test_checks_family.c
+++ test/test_checks_family.c
@@ -9,13 +9,6 @@
 Suite *create_test_suite(void);
 
 
-
-/* ******************************
- *      Unchecked fixtures      *
- * ******************************/
-
-
-
 /* ******************************
  *          Unit tests         *
  * ******************************/
@@ -63,52 +56,59 @@ START_TEST (mendel_type_2) {
 END_TEST
 
 START_TEST (mendel_type_3) {
-    fail_if(check_mendel("20", 0, 0, 1, 1, 0, 0, FEMALE) != 3,    "00x11 -> 00 is a mendel error 3");
-    fail_if(check_mendel("20", 0, 1, 1, 1, 0, 0, FEMALE) != 3,    "01x11 -> 00 is a mendel error 3");
-    fail_if(check_mendel("20", 1, 0, 1, 1, 0, 0, FEMALE) != 3,    "10x11 -> 00 is a mendel error 3");
-    fail_if(check_mendel("20", 1, 1, 1, 1, 0, 0, FEMALE) != 3,    "11x11 -> 00 is a mendel error 3");
+    fail_if(check_mendel("20", 0, 0, 1, 1, 0, 0, FEMALE) != 3,  "00x11 -> 00 is a mendel error 3");
+    fail_if(check_mendel("20", 0, 1, 1, 1, 0, 0, FEMALE) != 3,  "01x11 -> 00 is a mendel error 3");
+    fail_if(check_mendel("20", 1, 0, 1, 1, 0, 0, FEMALE) != 3,  "10x11 -> 00 is a mendel error 3");
 }
 END_TEST
 
 START_TEST (mendel_type_4) {
-    fail_if(check_mendel("20", 1, 1, 0, 0, 0, 0, FEMALE) != 3,    "11x00 -> 00 is a mendel error 4");
-    fail_if(check_mendel("20", 1, 1, 0, 1, 0, 0, FEMALE) != 3,    "11x01 -> 00 is a mendel error 4");
-    fail_if(check_mendel("20", 1, 1, 1, 0, 0, 0, FEMALE) != 3,    "11x10 -> 00 is a mendel error 4");
-    fail_if(check_mendel("20", 1, 1, 1, 1, 0, 0, FEMALE) != 4,    "11x11 -> 00 is a mendel error 4");
+    fail_if(check_mendel("20", 1, 1, 0, 0, 0, 0, FEMALE) != 4,  "11x00 -> 00 is a mendel error 4");
+    fail_if(check_mendel("20", 1, 1, 0, 1, 0, 0, FEMALE) != 4,  "11x01 -> 00 is a mendel error 4");
+    fail_if(check_mendel("20", 1, 1, 1, 0, 0, 0, FEMALE) != 4,  "11x10 -> 00 is a mendel error 4");
 }
 END_TEST
 
 START_TEST (mendel_type_5) {
-    
+    fail_if(check_mendel("20", 1, 1, 1, 1, 0, 0, MALE) != 5,    "11x11 -> 00 is a mendel error 5");
 }
 END_TEST
 
 START_TEST (mendel_type_6) {
-    
+    fail_if(check_mendel("20", 0, 0, 0, 1, 1, 1, FEMALE) != 6,  "00x01 -> 11 is a mendel error 6");
+    fail_if(check_mendel("20", 0, 0, 1, 0, 1, 1, FEMALE) != 6,  "00x10 -> 11 is a mendel error 6");
+    fail_if(check_mendel("20", 0, 0, 1, 1, 1, 1, FEMALE) != 6,  "00x11 -> 11 is a mendel error 6");
 }
 END_TEST
 
 START_TEST (mendel_type_7) {
-    
+    fail_if(check_mendel("20", 0, 1, 0, 0, 1, 1, MALE) != 7,    "01x00 -> 11 is a mendel error 7");
+    fail_if(check_mendel("20", 1, 0, 0, 0, 1, 1, MALE) != 7,    "10x00 -> 11 is a mendel error 7");
+    fail_if(check_mendel("20", 1, 1, 0, 0, 1, 1, MALE) != 7,    "11x00 -> 11 is a mendel error 7");
 }
 END_TEST
 
 START_TEST (mendel_type_8) {
-    
+    fail_if(check_mendel("20", 0, 0, 0, 0, 1, 1, MALE) != 8,    "00x00 -> 11 is a mendel error 8");
 }
 END_TEST
 
 START_TEST (mendel_type_9) {
-    
+    fail_if(check_mendel("X",  0, 0, 0, 0, 1, 1, MALE) != 9,    "00x00 -> 11 is a mendel error 9");
 }
 END_TEST
 
 START_TEST (mendel_type_10) {
-    
+    fail_if(check_mendel("X",  0, 0, 1, 1, 0, 0, MALE) != 10,   "00x11 -> 00 is a mendel error 10");
 }
 END_TEST
 
-
+START_TEST (mendel_ranges) {
+    fail_if(check_mendel("20", 0, 0, 0, 0, 1, 1, MALE) > 8,     "Chromosome no-X can't be an error of type 9/10");
+    fail_if(check_mendel("X",  0, 0, 0, 0, 1, 1, MALE) < 9,     "Male chromosome X can't be an error of type < 9");
+    fail_if(check_mendel("X",  0, 0, 0, 0, 1, 1, FEMALE) > 8,   "Female chromosome X can't be an error of type 9/10");
+}
+END_TEST
 
 /* ******************************
  *      Main entry point        *
@@ -139,9 +139,10 @@ Suite *create_test_suite(void)
     tcase_add_test(tc_mendel, mendel_type_8);
     tcase_add_test(tc_mendel, mendel_type_9);
     tcase_add_test(tc_mendel, mendel_type_10);
+    tcase_add_test(tc_mendel, mendel_ranges);
     
     // Add test cases to a test suite
-    Suite *fs = suite_create("Checks for families correctnes");
+    Suite *fs = suite_create("Checks for families' correctness");
     suite_add_tcase(fs, tc_mendel);
     
     return fs;
