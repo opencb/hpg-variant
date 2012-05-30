50baa5b0f364dd6860888fd8bf30b32c62181a8d
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-05-30 12:11:48 +0200

    Writing job.status
    Former-commit-id: 77b378701c1d3e6dcb767852e3c1ad289a11bddb

--- hpg_variant_utils.c
+++ hpg_variant_utils.c
@@ -1,6 +1,49 @@
 #include "hpg_variant_utils.h"
 
 
+char *find_configuration_file(int argc, char *argv[]) {
+    FILE *config_file = NULL;
+    char *config_filepath = NULL;
+    for (int i = 0; i < argc-1; i++) {
+        if (!strcmp("--config", argv[i])) {
+            config_filepath = argv[i+1];
+        }
+    }
+    if (!config_filepath) {
+        config_filepath = "hpg-variant.cfg";
+    }
+    
+    config_file = fopen(config_filepath, "r");
+    if (!config_file) {
+        LOG_FATAL("Configuration file can't be loaded!");
+    } else {
+        fclose(config_file);
+    }
+    
+    LOG_INFO_F("Configuration file = %s\n", config_filepath);
+    return config_filepath;
+}
+
+FILE* new_job_status_file(char* path) {
+    return fopen(path, "w");
+}
+
+
+void update_job_status_file(int percentage, FILE* file) {
+    if (percentage < 100) {
+        fprintf(file, "%d\n", percentage);
+    } else {
+        fprintf(file, "100\tDone\n");
+    }
+}
+
+void close_job_status_file(FILE* file) {
+    if (file) {
+        fclose(file);
+    }
+}
+
+
 list_item_t** create_chunks(list_t* records, int max_chunk_size, int *num_chunks) {
     *num_chunks = (int) ceil((float) records->length / max_chunk_size);
     LOG_DEBUG_F("%d chunks of %d elements top\n", *num_chunks, max_chunk_size);
--- hpg_variant_utils.h
+++ hpg_variant_utils.h
@@ -8,9 +8,27 @@
 #include <log.h>
 
 
-// inline int int_cmp(int *a, int *b) {
-//     return *a - *b;
-// }
+/* ***********************
+ *     Initialization    *
+ * ***********************/
+
+char *find_configuration_file(int argc, char *argv[]);
+
+
+/* **********************
+ *    Job management    *
+ * **********************/
+
+FILE *new_job_status_file(char *path);
+
+void update_job_status_file(int percentage, FILE *file);
+
+void close_job_status_file(FILE *file);
+
+
+/* ***********************
+ *      Miscellaneous    *
+ * ***********************/
 
 /**
  * @brief Given a list of records, distributes them in chunks of similar size
@@ -23,5 +41,6 @@
  * by a maximum distance of max_chunk_size. These records will mark the beginning of each chunk.
  */
 list_item_t** create_chunks(list_t* records, int max_chunk_size, int *num_chunks);
+
 
 #endif
