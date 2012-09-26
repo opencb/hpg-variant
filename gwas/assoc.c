f30090d974f7c22dd9d767e0f9a94d134d718481
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-09-26 10:55:50 +0200

    GPLv2 license declaration

--- gwas/assoc.c
+++ gwas/assoc.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2012 Cristina Yenyxe Gonzalez Garcia (CGI-CIPF)
+ * Copyright (c) 2012 Ignacio Medina (CGI-CIPF)
+ *
+ * This file is part of hpg-variant.
+ *
+ * hpg-variant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hpg-variant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hpg-variant. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "assoc.h"
 
 void assoc_test(enum GWAS_task test_type, vcf_record_t **variants, int num_variants, family_t **families, int num_families, 
--- gwas/hardy_weinberg.h
+++ gwas/hardy_weinberg.h
@@ -18,6 +18,26 @@
  * along with hpg-variant. If not, see <http://www.gnu.org/licenses/>.
  */
 
+/*
+ * Copyright (c) 2012 Cristina Yenyxe Gonzalez Garcia
+ * Copyright (c) 2012 Ignacio Medina (CGI-CIPF)
+ *
+ * This file is part of hpg-variant.
+ *
+ * hpg-variant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * hpg-variant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hpg-variant. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #ifndef TRANSMISSION_DISEQUILIBRIUM_TEST_H
 #define TRANSMISSION_DISEQUILIBRIUM_TEST_H
 
@@ -39,27 +59,9 @@
 
 #include "checks_family.h"
 
-typedef struct {
-    char *chromosome;
-    char *reference;
-    char *alternate;
-    
-    unsigned long int position;
-    
-    int t1;
-    int t2;
-    double odds_ratio;
-    double chi_square;
-    double p_value;
-} tdt_result_t;
+int hardy_weinberg_test(vcf_record_t **variants, int num_variants, individual_t **individuals, int num_individuals, 
+                        cp_hashtable *sample_ids, list_t *output_list);
 
-int tdt_test(vcf_record_t **variants, int num_variants, family_t **families, int num_families, cp_hashtable *sample_ids, list_t *output_list);
-
-
-//tdt_result_t *tdt_result_new(char *chromosome, unsigned long int position, char *reference, char *alternate, double t1, double t2, double chi_square);
-tdt_result_t* tdt_result_new(char *chromosome, int chromosome_len, unsigned long int position, char *reference, int reference_len,
-                             char *alternate, int alternate_len, double t1, double t2, double chi_square);
-
-void tdt_result_free(tdt_result_t *result);
+individual_t **get_founders_from_families(family_t **families, int num_families, int *num_individuals);
 
 #endif
