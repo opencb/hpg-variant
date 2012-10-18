575f6723754aa352716b7e779f4cfc39d0df694f
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-10-18 17:19:52 +0200

    GPLv2 licensing

--- src/vcf-tools/stats/main_stats.c
+++ src/vcf-tools/stats/main_stats.c
@@ -1,3 +1,23 @@
+/*
+ * Copyright (c) 2012 Cristina Yenyxe Gonzalez Garcia (ICM-CIPF)
+ * Copyright (c) 2012 Ignacio Medina (ICM-CIPF)
+ *
+ * This file is part of hpg-variant.
+ *
+ * hpg-variant is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * hpg-variant is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with hpg-variant. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "stats.h"
 
 
