a897e584534209372ba871aeafec05bc51287c60
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-10-23 12:24:51 +0200

    Make filter tool use the shared options for filtering
    Former-commit-id: e891067bdeef9f12cc6cabe0b33b6d383d359b14

--- src/vcf-tools/filter/main_filter.c
+++ src/vcf-tools/filter/main_filter.c
@@ -80,15 +80,6 @@ int vcf_tool_filter(int argc, char *argv[], const char *configuration_file) {
 filter_options_t *new_filter_cli_options(void) {
     filter_options_t *options = (filter_options_t*) malloc (sizeof(filter_options_t));
     options->num_options = NUM_FILTER_OPTIONS;
-    
-    options->num_alleles = arg_int0(NULL, "alleles", NULL, "Filter: by number of alleles");
-    options->coverage = arg_int0(NULL, "coverage", NULL, "Filter: by minimum coverage");
-    options->quality = arg_int0(NULL, "quality", NULL, "Filter: by minimum quality");
-    options->maf = arg_dbl0(NULL, "maf", NULL, "Filter: by maximum MAF (minimum allele frequency)");
-    options->region = arg_str0(NULL, "region", NULL, "Filter: by a list of regions (chr1:start1-end1,chr2:start2-end2...)");
-    options->region_file = arg_file0(NULL, "region-file", NULL, "Filter: by a list of regions (read from a GFF file)");
-    options->snp = arg_str0(NULL, "snp", NULL, "Filter: by being a SNP or not");
-    
     options->save_rejected = arg_lit0(NULL, "save-rejected", "Write a file containing the rejected records");
     return options;
 }
@@ -96,53 +87,7 @@ filter_options_t *new_filter_cli_options(void) {
 
 filter_options_data_t *new_filter_options_data(filter_options_t *options, shared_options_t *shared_options) {
     filter_options_data_t *options_data = (filter_options_data_t*) calloc (1, sizeof(filter_options_data_t));
-    
-    filter_t *filter;
-    if (options->num_alleles->count > 0) {
-        filter = num_alleles_filter_new(*(options->num_alleles->ival));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("number of alleles filter = %d\n", *(options->num_alleles->ival));
-    }
-    if (options->coverage->count > 0) {
-        filter = coverage_filter_new(*(options->coverage->ival));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("minimum coverage filter = %d\n", *(options->coverage->ival));
-    }
-    if (options->quality->count > 0) {
-        filter = quality_filter_new(*(options->quality->ival));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("minimum quality filter = %d\n", *(options->quality->ival));
-    }
-    if (options->maf->count > 0) {
-        filter = maf_filter_new(*(options->maf->dval));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("maximum MAF = %.3f\n", ((maf_filter_args*)filter->args)->max_maf);
-    }
-    if (options->snp->count > 0) {
-        if (!strcmp(*(options->snp->sval), "exclude")) {
-            filter = snp_filter_new(0);
-        } else {
-            filter = snp_filter_new(1);
-        }
-//         filter = snp_filter_new(strdup(*(options->snp->sval)));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("snp filter to %s SNPs\n", *(options->snp->sval));
-    }
-    if (options->region->count > 0) {
-        filter = region_exact_filter_new(strdup(*(options->region->sval)), 0,
-                                            *(shared_options->host_url->sval), *(shared_options->species->sval), *(shared_options->version->sval));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("regions = %s\n", *(options->region->sval));
-    } 
-    if (options->region_file->count > 0) {
-        filter = region_exact_filter_new(strdup(*(options->region->sval)), 1, 
-                                            *(shared_options->host_url->sval), *(shared_options->species->sval), *(shared_options->version->sval));
-        options_data->chain = add_to_filter_chain(filter, options_data->chain);
-        LOG_DEBUG_F("regions file = %s\n", *(options->region->sval));
-    }
-    
     options_data->save_rejected = (options->save_rejected->count > 0);
-
     return options_data;
 }
 
