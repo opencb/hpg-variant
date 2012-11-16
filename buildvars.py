5de81a8c83f2b8ef2ae1250d76be4c760eafc028
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-11-16 12:51:46 +0100

    Full construction system using SCons
    Former-commit-id: 20be81f6fbee12d39a57dcad87feecbcf681f83e

---
+++ buildvars.py
@@ -0,0 +1,9 @@
+import os
+
+# Build variables
+ARGTABLE_INCLUDE_PATH = os.environ.get('HOME', '') + '/appl/bioinfo-c/libs/ext/argtable2-13/src'
+ARGTABLE_LIBRARY_PATH = os.environ.get('HOME', '') + '/appl/bioinfo-c/libs/ext/argtable2-13/src'
+
+CPROPS_INCLUDE_PATH = '/usr/local/include'
+CPROPS_LIBRARY_PATH = '/usr/local/lib'
+ 
