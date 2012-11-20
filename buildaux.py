b868b96097bfc9e43d8b8210d09caaba6a589b9e
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-11-20 15:03:00 +0100

    Tarball tested under Fedora and Debian
    Former-commit-id: b21916a1c8887f40b23de221d7722a72421b45dd

--- buildaux.py
+++ buildaux.py
@@ -1,12 +1,35 @@
 import SCons
 
+def FindHeaderFiles(self, target):
+    def _find_sources(tgt, src, all):
+        for item in tgt:
+            if SCons.Util.is_List(item):
+                _find_sources(item, src, all)
+            else:
+                if item.abspath in all:
+                    continue
+                    
+                all[item.abspath] = True
+
+                if item.path.endswith('.h'):
+                    if not item.exists():
+                        item = item.srcnode()
+                    src.append(item.abspath)
+                else:
+                    lst = item.children(scan=1)
+                    _find_sources(lst, src, all)
+
+    sources = []
+    _find_sources(target, sources, {})
+    return sources
+
+
 def FindAllSourceFiles(self, target):
     def _find_sources(tgt, src, all):
         for item in tgt:
             if SCons.Util.is_List(item):
                 _find_sources(item, src, all)
             else:
-                #print item.abspath
                 if item.abspath in all:
                     continue
                     
@@ -20,14 +43,6 @@ def FindAllSourceFiles(self, target):
                     if not item.exists():
                         item = item.srcnode()
                     src.append(item.abspath)
-                elif item.path.endswith('SConstruct'):
-                    if not item.exists():
-                        item = item.srcnode()
-                    src.append(item.abspath)
-                elif item.path.endswith('SConscript'):
-                    if not item.exists():
-                        item = item.srcnode()
-                    src.append(item.abspath)
                 else:
                     lst = item.children(scan=1)
                     _find_sources(lst, src, all)
@@ -36,6 +51,7 @@ def FindAllSourceFiles(self, target):
     _find_sources(target, sources, {})
     return sources
 
-    
+        
 from SCons.Script.SConscript import SConsEnvironment # just do this once
+SConsEnvironment.FindHeaderFiles = FindHeaderFiles
 SConsEnvironment.FindAllSourceFiles = FindAllSourceFiles
