bfe584d4bfe919e565a443258e9c019de43525b5
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-12-13 18:00:15 +0100

    Write header of epistasis dataset
    Former-commit-id: 021557d6d917312e2c22554d7adfb948ff6fba7c

--- src/epistasis/dataset_creator.h
+++ src/epistasis/dataset_creator.h
@@ -22,7 +22,7 @@ int create_dataset_from_vcf(shared_options_data_t* shared_options_data);
 
 static int flatten_phenotypes(vcf_file_t *vcf, ped_file_t *ped, int *num_affected, int *num_unaffected);
 
-static int *get_individual_phenotypes(vcf_file_t *vcf, ped_file_t *ped, int *num_affected, int *num_unaffected);
+static uint8_t *get_individual_phenotypes(vcf_file_t *vcf, ped_file_t *ped);
 
 static individual_t **sort_individuals(vcf_file_t *vcf, ped_file_t *ped);
 
