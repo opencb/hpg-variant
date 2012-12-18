662a936026714340d064001b513b9dfd07a1d7d8
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2012-12-18 15:38:32 +0100

    Filter by percentage of missing values
    Former-commit-id: 714aeab5fdde643aa72695d000619241d96b57aa

--- src/vcf-tools/merge/main_merge.c
+++ src/vcf-tools/merge/main_merge.c
@@ -75,7 +75,7 @@ int vcf_tool_merge(int argc, char *argv[], const char *configuration_file, array
 
     free_merge_options_data(options_data);
     free_shared_options_data(shared_options_data);
-    arg_freetable(argtable, merge_options->num_options + shared_options->num_options + 1 - 9);
+    arg_freetable(argtable, merge_options->num_options + shared_options->num_options - 9);
 
     return 0;
 }
