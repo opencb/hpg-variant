81a089194fa659176c2d4d8405d0f1e2c9f42e3a
Cristina Yenyxe Gonzalez Garcia <cgonzalez@cipf.es>
2013-03-25 14:53:16 +0100

    Gene filter option added to hpg-var-effect, some memleaks removed

--- src/effect/effect_options_parsing.c
+++ src/effect/effect_options_parsing.c
@@ -33,8 +33,6 @@ int read_effect_configuration(const char *filename, effect_options_t *effect_opt
         return CANT_READ_CONFIG_FILE;
     }
 
-    const char *tmp_string;
-    
     // Read number of threads that will make request to the web service
     ret_code = config_lookup_int(config, "effect.num-threads", shared_options->num_threads->ival);
     if (ret_code == CONFIG_FALSE) {
@@ -88,7 +86,7 @@ void **parse_effect_options(int argc, char *argv[], effect_options_t *effect_opt
 }
 
 void **merge_effect_options(effect_options_t *effect_options, shared_options_t *shared_options, struct arg_end *arg_end) {
-    size_t opts_size = effect_options->num_options + shared_options->num_options + 1 - 1;
+    size_t opts_size = effect_options->num_options + shared_options->num_options;
     void **tool_options = malloc (opts_size * sizeof(void*));
     // Input/output files
     tool_options[0] = shared_options->vcf_filename;
@@ -108,24 +106,25 @@ void **merge_effect_options(effect_options_t *effect_options, shared_options_t *
     tool_options[8] = shared_options->quality;
     tool_options[9] = shared_options->maf;
     tool_options[10] = shared_options->missing;
-    tool_options[11] = shared_options->region;
-    tool_options[12] = shared_options->region_file;
-    tool_options[13] = shared_options->snp;
+    tool_options[11] = shared_options->gene;
+    tool_options[12] = shared_options->region;
+    tool_options[13] = shared_options->region_file;
+    tool_options[14] = shared_options->snp;
     
     // Configuration file
-    tool_options[14] = shared_options->config_file;
+    tool_options[15] = shared_options->config_file;
     
     // Advanced configuration
-    tool_options[15] = shared_options->host_url;
-    tool_options[16] = shared_options->version;
-    tool_options[17] = shared_options->max_batches;
-    tool_options[18] = shared_options->batch_lines;
-    tool_options[19] = shared_options->batch_bytes;
-    tool_options[20] = shared_options->num_threads;
-    tool_options[21] = shared_options->entries_per_thread;
-    tool_options[22] = shared_options->mmap_vcf_files;
+    tool_options[16] = shared_options->host_url;
+    tool_options[17] = shared_options->version;
+    tool_options[18] = shared_options->max_batches;
+    tool_options[19] = shared_options->batch_lines;
+    tool_options[20] = shared_options->batch_bytes;
+    tool_options[21] = shared_options->num_threads;
+    tool_options[22] = shared_options->entries_per_thread;
+    tool_options[23] = shared_options->mmap_vcf_files;
     
-    tool_options[23] = arg_end;
+    tool_options[24] = arg_end;
     
     return tool_options;
 }
